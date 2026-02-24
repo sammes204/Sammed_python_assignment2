1174d8ae4caaada5be9b0f65e0882a4ca6c00fc4
sammes204 <sammes204@gmail.com>
2026-02-24 17:41:20 +0530

    Implement binary search algorithm in BinarySearch.py

---
+++ BinarySearch.py
@@ -0,0 +1,15 @@
+def binary_search(arr, target, left, right):
+    if left > right:
+        return -1
+    
+    mid = (left + right) // 2
+    
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] > target:
+        return binary_search(arr, target, left, mid - 1)
+    else:
+        return binary_search(arr, target, mid + 1, right)
+
+arr = [1,2,3,4,5,6,7]
+print(binary_search(arr, 5, 0, len(arr)-1))
